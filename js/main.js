291ae9811bd8999b54658674cd81ad06bee7b3db
Tikvah <T.harr@live.com>
2016-09-27 13:43:43 -0700

    Project1 Jquery click effects

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
-$(document).ready(function(){
-    $("p").click(function(){
-        $(this).hide();
-    });
+$(function(){
+
+  //Clicking on the Save button will activate Successful Save message
+  $("#h1").click(function(e){
+    $("#message").html("click one");
+ });
+ $("#h2").click(function(e){
+    $("#message").html("click two");
+  });
+ $("#h3").click(function(e){
+    $("#message").html("click three");
+});
 });
