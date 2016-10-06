6b53d7f77323380592c79e87478274cb3107b990
Tikvah <T.harr@live.com>
2016-10-06 10:15:45 -0700

    Project images full toggle effect

--- js/main.js
+++ js/main.js
@@ -1,13 +1,27 @@
 $(function(){
 
-$("#h1").click(function() {
-    $("#img1").attr("src", "images/image4.jpg");
+    $("#img1").click(function(){
+        if ($(this).attr("src") == "images/image1.jpg") {
+            $(this).attr("src","images/image4.jpg");
+        }
+        else if ($(this).attr("src") == "images/image4.jpg") {
+            $(this).attr("src","images/image1.jpg");
+        }
+    });
+    $("#img2").click(function(){
+        if ($(this).attr("src") == "images/image2.jpg") {
+            $(this).attr("src","images/image5.jpg");
+        }
+        else if ($(this).attr("src") == "images/image5.jpg") {
+            $(this).attr("src","images/image2.jpg");
+        }
+    });
+$("#img3").click(function(){
+        if ($(this).attr("src") == "images/image3.jpg") {
+            $(this).attr("src","images/image6.jpg");
+        }
+        else if ($(this).attr("src") == "images/image6.jpg") {
+            $(this).attr("src","images/image3.jpg");
+        }
+    });
 });
- $("#h2").click(function(e){
-    $("#img2").attr("src", "images/image5.jpg");
-  });
- $("#h3").click(function(e){
-  $("#img3").attr("src", "images/image6.jpg");
-});
-});
-
